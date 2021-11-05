93287c96ca86251b31de41cac8e098b7eb5124fc
EvelyneCalista <71305979+EvelyneCalista@users.noreply.github.com>
2021-11-05 10:45:32 +0800

    Update train.sh

--- HW1_BirdClassification/train.sh
+++ HW1_BirdClassification/train.sh
@@ -1,4 +1,3 @@
-# CUDA_VISIBLE_DEVICES=0 \
 python -m torch.distributed.launch --nproc_per_node=1 train.py \
 --name 'mybird_trainv13_linear_changetransform_vitl16_20000e_batch6_reproduce' \
 --dataset 'myBirds' \
@@ -12,6 +11,5 @@ python -m torch.distributed.launch --nproc_per_node=1 train.py \
 --decay_type "linear" \
 --fp16 \
 --img_dir 'trainimage_path/training_images' \
---img_order 'testing_img_order.txt' \
 --img_labels 'train.txt'  \
 --img_val 'val.txt' 
